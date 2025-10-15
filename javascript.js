39aa86fbf665e707dd2ee98c852efe5079ef5e12
commit <minhdara244@gmail.com>
2025-10-15 14:57:46 -0500

    commit

--- javascript.js
+++ javascript.js
@@ -45,62 +45,107 @@
 //
 // timesTable(4)
 
+// function getRowsDesired() {
+//     return Number(document.getElementById("rows").value)
+// }
+//
+// function getColumnsDesired() {
+//     return Number(document.getElementById("cols").value)
+// }
+//
+// function getHighlightValue() {
+//     let highlightValue = document.getElementById("highlight").value
+//
+//     if (highlightValue !== '') {
+//         return Number(highlightValue)
+//     }
+// }
+//
+//
+// function createWithHtmlTable(rows, cols, highlight) { // method declaration, has two parameters
+//     let string // define a variable called string
+//     if (rows && cols) { // if rows and columns were provided
+//         string = "<table>\n" // start building the HTML table
+//
+//         // first "for" loop to build the rows of the table
+//         for (let row = 1; row <= rows; row++) {
+//
+//             if (highlight && highlight === row) {
+//                 // if highlight "IS TRUTHY" and its value matches the row
+//                 // we're on, then do this next
+//                 string += "<tr class='highlight'>\n"
+//             } else {
+//                 // otherwise just start a regular HTML row with no class
+//                 string += "<tr>\n" // start building an HTML row
+//             }
+//
+//             // second "for" loop to build the columns of EACH row
+//             for (let col = 1; col <= cols; col++) {
+//
+//                 if (highlight && highlight === col) {
+//                     string += "<td class='highlight'>\n"
+//                 } else {
+//                     string += "<td>" // start building a single "cell" or column in the HTML row
+//                 }
+//
+//
+//                 string += row * col // populate the contents of the HTML cell
+//                 string += "</td>\n" // complete the HTML cell
+//             }
+//             string += "</tr>\n" // complete the HTML row
+//         }
+//
+//         string += "</table>\n" // complete the HTML table
+//     } else { // I guess the method wasn't provided either rows or columns
+//         string = 'Provide some inputs' // set value of "string" variable to "Provide some inputs"
+//     }
+//
+//     document.querySelector("section.container > section#output").innerHTML = string
+//     // document.getElementById("output").innerHTML = string; // WRITE the string we've just built to an element
+//     // in the HTML document whose ID is "output"
+// }
 function getRowsDesired() {
-    return Number(document.getElementById("rows").value)
+    return Number(document.getElementById("rows").value);
 }
 
 function getColumnsDesired() {
-    return Number(document.getElementById("cols").value)
+    return Number(document.getElementById("cols").value);
 }
 
 function getHighlightValue() {
-    let highlightValue = document.getElementById("highlight").value
-
+    let highlightValue = document.getElementById("highlight").value;
     if (highlightValue !== '') {
-        return Number(highlightValue)
+        return Number(highlightValue);
     }
 }
 
-
-function createWithHtmlTable(rows, cols, highlight) { // method declaration, has two parameters
-    let string // define a variable called string
-    if (rows && cols) { // if rows and columns were provided
-        string = "<table>\n" // start building the HTML table
-
-        // first "for" loop to build the rows of the table
+function createWithHtmlTable(rows, cols, highlight) {
+    let string;
+    if (rows && cols) {
+        string = "<table>\n";
         for (let row = 1; row <= rows; row++) {
-
             if (highlight && highlight === row) {
-                // if highlight "IS TRUTHY" and its value matches the row
-                // we're on, then do this next
-                string += "<tr class='highlight'>\n"
+                string += "<tr class='highlight'>\n";
             } else {
-                // otherwise just start a regular HTML row with no class
-                string += "<tr>\n" // start building an HTML row
+                string += "<tr>\n";
             }
 
-            // second "for" loop to build the columns of EACH row
             for (let col = 1; col <= cols; col++) {
-
                 if (highlight && highlight === col) {
-                    string += "<td class='highlight'>\n"
+                    string += "<td class='highlight'>";
                 } else {
-                    string += "<td>" // start building a single "cell" or column in the HTML row
+                    string += "<td>";
                 }
 
-
-                string += row * col // populate the contents of the HTML cell
-                string += "</td>\n" // complete the HTML cell
+                string += row * col;
+                string += "</td>\n";
             }
-            string += "</tr>\n" // complete the HTML row
+            string += "</tr>\n";
         }
-
-        string += "</table>\n" // complete the HTML table
-    } else { // I guess the method wasn't provided either rows or columns
-        string = 'Provide some inputs' // set value of "string" variable to "Provide some inputs"
+        string += "</table>\n";
+    } else {
+        string = 'Provide some inputs';
     }
 
-    document.querySelector("section.container > section#output").innerHTML = string
-    // document.getElementById("output").innerHTML = string; // WRITE the string we've just built to an element
-    // in the HTML document whose ID is "output"
+    document.querySelector("section.container > section#output").innerHTML = string;
 }
